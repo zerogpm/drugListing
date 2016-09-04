94907d3db181c3126778717c5e69e23d7afec6b2
Chris Su <zerogpm@gmail.com>
2016-09-04 13:39:53 -0400

    project init

---
+++ app/Http/routes.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function() {
+   return 'about';
+});
+
+
+Route::group(['prefix' => 'api/v1'], function() {
+    Route::resource('meeting', 'MeetingController', [
+        'expect' => ['edit', 'create']
+    ]);
+
+    Route::resource('meeting/registration', 'RegistrationController', [
+        'only' => ['store', 'destroy']
+    ]);
+
+    Route::post('user',[
+        'uses' => 'AuthController@store'
+    ]);
+
+    Route::post('user/signin', [
+        'uses' => 'AuthController@sigin'
+    ]);
+});
+
